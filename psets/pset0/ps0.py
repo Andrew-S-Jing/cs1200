788795297682ad60cda5b06ae59c3116220be6e4
Andrew Jing <andrew_jing@college.harvard.edu>
2026-09-18 15:48:17 -0400

    ps0 code

--- psets/pset0/ps0.py
+++ psets/pset0/ps0.py
@@ -14,7 +14,8 @@ class BinaryTree:
         :param root: the root of the binary tree
         """
         self.root: BTvertex = root
- 
+
+
 class BTvertex:
     def __init__(self, key):
         """
@@ -31,26 +32,34 @@ class BTvertex:
 # Problem 1a
 #
 
+
 # Input: BTvertex v, the root of a BinaryTree of size n
 # Output: Up to you
 # Side effect: sets the size of each vertex n in the
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
-def calculate_sizes(v):
-    # Your code goes here
-    pass 
+def calculate_sizes(v: BTvertex | None) -> int:
+    if v is None:
+        return 0
+    v.size = 1 + calculate_sizes(v.left) + calculate_sizes(v.right)
+    return v.size
 
 
 #
 # Problem 1c
 #
 
-# Input: a positive integer t, 
+# Input: a positive integer t,
 # ...BTvertex v, the root of a BinaryTree of size n >= 2t+1
-# Output: BTvertex, descendent of v such that its size is between 
+# Output: BTvertex, descendent of v such that its size is between
 # ... t and 2t-1 (inclusive)
-# Runtime: O(h) 
+# Runtime: O(h)
 
-def FindDescendantOfSize(t, v):
-    # Your code goes here 
-    pass 
+
+def FindDescendantOfSize(t: int, v: BTvertex) -> BTvertex:
+    assert v is not None and v.size >= t
+    if v.size <= 2 * t - 1:
+        return v
+    if v.left is None or v.left.size < t:
+        return FindDescendantOfSize(t, v.right)
+    return FindDescendantOfSize(t, v.left)
